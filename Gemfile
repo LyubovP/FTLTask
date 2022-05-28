32a80f2bce19ada8df91ad391bb5879a131790be
LyubovP <lyubovpidoshva@gmail.com>
2022-05-28 05:22:55 +0000

    Added Card

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.4', require: false
 gem 'devise'
 gem 'bootstrap', '~> 5.1.0'
+gem 'simple_form'
+gem 'font-awesome-sass'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ app/controllers/cards_controller.rb
@@ -0,0 +1,52 @@
+class CardsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_card, except: [:index, :new, :create, :search]
+  
+  def show
+  end
+  
+  def new
+    @card = collection.build
+  end
+
+  def create
+    @card = collection.build(card_params)
+
+    if @card.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @card.update(card_params)
+      flash[:notice] = "Card has been updated"
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @card.destroy
+    flash[:notice] = "Card has been deleted"
+    redirect_to root_path
+  end
+
+  private
+
+  def collection
+    current_user.cards
+  end
+  
+  def find_card
+    @card = collection.find(params[:id])
+  end
+
+  def card_params
+    params.require(:card).permit(:title, :description, :category, :currency)
+  end
+end
