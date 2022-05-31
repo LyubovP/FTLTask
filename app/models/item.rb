074c2a45d02ec62cb371683df4e62629c3d27baa
LyubovP <lyubovpidoshva@gmail.com>
2022-05-31 12:47:38 +0000

    Added category to Items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :card
   
+  enum category: [:Traveling, :Clothing, :Taxi, :Cafes, :Shops, :Other]
+  
   validates :title, presence: true, length: { in: 3..128 }
   validates :price, presence: true
-  
+  validates :category, inclusion: { in: Item::categories }
 end
